82a47b4d9385339f8a767915b3f53d48e6e2bc6a
ajay00764 <kharatajay314@gmail.com>
2024-03-12 18:44:40 +0530

    update website

--- src/app/features/components/about/about.component.ts
+++ src/app/features/components/about/about.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  downloadCV() {
+    let a = document.createElement('a');
+    a.href = "assets/Ajay-Kharat-Resume.pdf"
+    a.download = "Ajay-Kharat-Resume.pdf"
+    a.click();
+  }
+
 }
--- src/app/features/components/home/home.component.ts
+++ src/app/features/components/home/home.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  downloadCV() {
+    let a = document.createElement('a');
+    a.href = "assets/Ajay-Kharat-Resume.pdf"
+    a.download = "Ajay-Kharat-Resume.pdf"
+    a.click();
+  }
+
 }
